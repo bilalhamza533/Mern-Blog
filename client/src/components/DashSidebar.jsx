da5475beeb77f569ae69364e96f39281f3c4b96c
Bilal Hamza <bilalhamzacaptain.work@gmail.com>
2024-10-26 02:26:22 +0500

    completed profile photo upload functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -18,13 +18,14 @@ export default function DashSidebar() {
         <Sidebar.Items>
             <Sidebar.ItemGroup>
                 <Link to={'/dashboard?tab=profile'}>
-                <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"User"} labelColor='dark'>
+                <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={"User"} 
+                as='div' labelColor='dark'>
                     Profile
                 </Sidebar.Item>
+                </Link>
                 <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
                     Sign Out
-                </Sidebar.Item>
-                </Link>
+                </Sidebar.Item> 
             </Sidebar.ItemGroup>
         </Sidebar.Items>
     </Sidebar>
